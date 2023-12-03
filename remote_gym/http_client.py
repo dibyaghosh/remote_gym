1f76456ce0c4e0c9bb2800d8f1705061692f6248
Dibya Ghosh <dibya.ghosh@berkeley.edu>
2023-12-03 00:18:12 +0000

    Way to get attributes on env

--- remote_gym/http_client.py
+++ remote_gym/http_client.py
@@ -1,56 +1,94 @@
-import requests
-import urllib.parse
-import numpy as np
-import gym
 import logging
+import urllib.parse
+
+import gym
+import numpy as np
+import requests
+
 from .serialization import deserialize_space, loads
 
-class RemoteEnv(gym.Env):
-    """ A simple client for the Gym server. """
-    
-    @classmethod
-    def make(cls, env_name, url='http://localhost:8000', **kwargs):
-        logging.warning(f'Creating remote environment {env_name} with kwargs {kwargs}')
-        response = loads(requests.post(
-            urllib.parse.urljoin(url, 'make'),
-            json={'env_name': env_name, 'env_kwargs': kwargs}
-        ).json())
-        logging.warning(f'Created env with instance_id {response["instance_id"]}')
-        return cls(response['instance_id'], url=url)
 
-    def __init__(self, instance_id, url='http://localhost:8000'):
+class ServerAttribute:
+    def __init__(self, env: "RemoteEnv", attr: str):
+        self.env = env
+        self.attr = attr
+
+    def __repr__(self) -> str:
+        return f"env.{self.attr}"
+
+    def __getattr__(self, attr):
+        v = self.env.query_endpoint("get_attr", {"attr": f"{self.attr}.{attr}"})
+        if "attr" in v:
+            return v["attr"]
+        else:
+            return ServerAttribute(self.env, f"{self.attr}.{attr}")
+
+    def __call__(self, *args, **kwargs):
+        return self.env.query_endpoint(
+            "get_attr", {"attr": self.attr, "args": args, "kwargs": kwargs}
+        )
+
+
+class RemoteEnv(gym.Env):
+    """A simple client for the Gym server."""
+
+    @classmethod
+    def make(cls, env_name, url="http://localhost:8000", **kwargs):
+        logging.warning(f"Creating remote environment {env_name} with kwargs {kwargs}")
+        response = loads(
+            requests.post(
+                urllib.parse.urljoin(url, "make"),
+                json={"env_name": env_name, "env_kwargs": kwargs},
+            ).json()
+        )
+        logging.warning(f'Created env with instance_id {response["instance_id"]}')
+        return cls(response["instance_id"], url=url)
+
+    def __init__(self, instance_id, url="http://localhost:8000"):
         self.url = url
         self.instance_id = instance_id
 
         response = self.info()
-        self.observation_space = deserialize_space(response['observation_space'])
-        self.action_space = deserialize_space(response['action_space'])
-        logging.warning(f'Connected to remote environment with observation space {self.observation_space} and action space {self.action_space}')
+        self.observation_space = deserialize_space(response["observation_space"])
+        self.action_space = deserialize_space(response["action_space"])
+        logging.warning(
+            f"Connected to remote environment with observation space {self.observation_space} and action space {self.action_space}"
+        )
 
-    def query_endpoint(self, endpoint, data, method='POST'):
-        if method == 'POST':
+    def query_endpoint(self, endpoint, data, method="POST"):
+        if method == "POST":
             response = requests.post(
                 urllib.parse.urljoin(self.url, endpoint),
-                json={**data, 'instance_id': self.instance_id}
+                json={**data, "instance_id": self.instance_id},
             )
-        elif method == 'GET':
+        elif method == "GET":
             response = requests.get(
                 urllib.parse.urljoin(self.url, endpoint),
-                params={**data, 'instance_id': self.instance_id}
+                params={**data, "instance_id": self.instance_id},
             )
-        
+
         return loads(response.json())
-    
+
     def info(self):
-        return self.query_endpoint('info', {}, method='GET')
-    
+        return self.query_endpoint("info", {}, method="GET")
+
     def reset(self):
-        response = self.query_endpoint('reset', {})
-        return response['observation']
+        response = self.query_endpoint("reset", {})
+        return response["observation"]
 
     def step(self, action):
-        response = self.query_endpoint('step', {'action': action})
-        return response['next_observation'], response['reward'], response['done'], response['info']
+        response = self.query_endpoint("step", {"action": action})
+        return (
+            response["next_observation"],
+            response["reward"],
+            response["done"],
+            response["info"],
+        )
+
+    def __getattr__(self, attr):
+        response = self.query_endpoint("get_attr", {"attr": attr})
+        return response["attr"]
+
 
 # Create an env either by env = RemoteEnv.make(env_name) or by env = gym.make('remote-env-v0', env_name)
-gym.register('remote-env-v0', RemoteEnv.make)
+gym.register("remote-env-v0", RemoteEnv.make)
--- remote_gym/http_server.py
+++ remote_gym/http_server.py
@@ -1,23 +1,25 @@
 import json_numpy
-json_numpy.patch()
-from .serialization import serialize_space
-from .base_server import Server
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 
+json_numpy.patch()
+import logging
 from dataclasses import dataclass
 from typing import Any, Dict
 
 import uvicorn
-import logging
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+from .base_server import Server
+from .serialization import serialize_space
 
 
 def json_response(obj):
     return JSONResponse(json_numpy.dumps(obj))
 
+
 class HttpServer:
-    """ A simple server for many Gym environments.
-    
+    """A simple server for many Gym environments.
+
     Use /make to create a new environment with a given name and kwargs.
         - Takes in {'env_name': str, 'env_kwargs': Dict[str, Any]}
         - Returns {'instance_id': int, 'observation_space': Space, 'action_space': Space}
@@ -30,60 +32,85 @@ class HttpServer:
         - Takes in {'instance_id': int, 'action': Any}
         - Returns {'next_observation': np.ndarray, 'reward': float, 'done': bool, 'info': Dict[str, Any]}
     """
+
     def __init__(self, base_server: Server = None):
         self.base_server = base_server or Server()
 
-    def run(self, port=8000, host='0.0.0.0'):
+    def run(self, port=8000, host="0.0.0.0"):
         self.app = FastAPI()
         self.app.post("/make")(self.create_env)
         self.app.post("/step")(self.step)
         self.app.post("/reset")(self.reset)
         self.app.get("/info")(self.get_info)
+        self.app.post("/get_attr")(self.get_attr)
 
-        uvicorn.run(self.app, host=host, port=port)  
-
+        uvicorn.run(self.app, host=host, port=port)
 
     def create_env(self, payload: Dict[Any, Any]):
-        env_name = payload['env_name']
-        kwargs = payload['env_kwargs']
+        env_name = payload["env_name"]
+        kwargs = payload["env_kwargs"]
         id, env = self.base_server.create_env(env_name, kwargs)
-        return json_response({
-            'instance_id': id,
-            'observation_space': serialize_space(env.observation_space),
-            'action_space': serialize_space(env.action_space),
-        })
+        return json_response(
+            {
+                "instance_id": id,
+                "observation_space": serialize_space(env.observation_space),
+                "action_space": serialize_space(env.action_space),
+            }
+        )
 
     def get_info(self, instance_id: str):
         env = self.base_server.get_env(instance_id)
-        return json_response({
-            'observation_space': serialize_space(env.observation_space),
-            'action_space': serialize_space(env.action_space),
-        })
+        return json_response(
+            {
+                "observation_space": serialize_space(env.observation_space),
+                "action_space": serialize_space(env.action_space),
+            }
+        )
 
     def step(self, payload: Dict[Any, Any]):
-        instance_id = payload['instance_id']
-        action = payload['action']
+        instance_id = payload["instance_id"]
+        action = payload["action"]
         ns, r, done, info = self.base_server.step(instance_id, action)
-        return json_response({
-            'next_observation': ns,
-            'reward': r,
-            'done': done,
-            'info': info,
-        })
-    
+        return json_response(
+            {
+                "next_observation": ns,
+                "reward": r,
+                "done": done,
+                "info": info,
+            }
+        )
+
     def reset(self, payload: Dict[Any, Any]):
-        instance_id = payload['instance_id']
+        instance_id = payload["instance_id"]
         o = self.base_server.reset(instance_id)
-        return json_response({'observation': o})
+        return json_response({"observation": o})
+
+    def get_attr(self, payload: Dict[Any, Any]):
+        instance_id = payload["instance_id"]
+        attr = payload["attr"]
+        env = self.base_server.get_env(instance_id)
+        nested = attr.split(".")
+        return_value = env
+        for attr in nested:
+            return_value = getattr(return_value, attr)
+
+        if "kwargs" in payload or "args" in payload:
+            kwargs = payload.get("kwargs", {})
+            args = payload.get("args", [])
+            return_value = return_value(*args, **kwargs)
+        return json_response({"attr": return_value})
+
 
 def main():
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", help="Host to run on", default="0.0.0.0", type=str)
     parser.add_argument("--port", help="Port to run on", default=8000, type=int)
     args = parser.parse_args()
     server = HttpServer()
     server.run(args.port, args.host)
+
 
 if __name__ == "__main__":
     main()
